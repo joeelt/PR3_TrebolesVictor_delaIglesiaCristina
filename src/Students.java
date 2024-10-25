3a813a31da3b38752c741bdd595f6cd3e23d330f
joeelt <j.treboles.pilataxi@gmail.com>
2024-10-25 11:53:35 +0200

    class Students.java

---
+++ src/Students.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Students {
+
+    private class Node{
+        BinaryTree info;
+        Node next;
+
+        Node(BinaryTree info) {
+            this.info = info;
+            this.next = null;
+        }
+        private Node first;
+    }
+
+    public Students(){
+
+    }
+
+    public void addStudent(BinaryTree nouEstudiant){
+
+    }
+
+    public void removeStudent(String name){
+
+    }
+
+    public BinaryTree getStudent(String name){
+        return null;
+    }
+
+    public ArrayList<String> getAllStudents(){
+        return null;
+    }
+}
